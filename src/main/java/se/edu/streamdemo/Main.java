3dc0c86122a0e0755905fab21dc71eb301c7d980
marken9 <177712014+marken9@users.noreply.github.com>
2026-02-20 17:08:43 +0800

    Delete old welcome

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -9,8 +9,10 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Task manager (using streams)");
-        Datamanager dataManager = new Datamanager("./data/data.txt");
+        printWelcomeMessage();
+        Datamanager dataManager = new Datamanager("./data/data.txt"); // relative path
+        // "C:\\Users\\dcsaksh\\Desktop\\ip\\data\\data.txt" <<< absolute path
+        // /home/username/ip/data/data.txt
         ArrayList<Task> tasksData = dataManager.loadData();
 
         System.out.println("Printing all data ...");
